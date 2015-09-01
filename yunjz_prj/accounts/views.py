03e1ce7c9b11c5af89f288c48c9796fe5a09ac35
heibanke <heibanke@aliyun.com>
2015-09-01 13:24:08 +0800

    shiyan2-v0.1

--- yunjz_prj/accounts/views.py
+++ yunjz_prj/accounts/views.py
@@ -1,30 +1,32 @@
 #coding=utf-8
 #django package
 from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.contrib.auth.models import User  
 from django.template.context import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login as auth_login ,logout as auth_logout
 def index(request):
-    return render_to_response("accounts/index.html",)
+    username = "heibanke"
+    return render(request,"accounts/index.html",{"username":username})
 
 def register(request):
     '''注册视图'''
     if request.method=="POST":
         #注册完毕 直接登陆
         return HttpResponseRedirect("/accounts/index")            
-    return render_to_response("accounts/register.html",)
+    return render(request,"accounts/register.html")
 
 	
 def login(request):
     '''登陆视图'''
     template_var={}    
     if request.method == 'POST':
-        template_var={"error":"you must first register"}                 
-    return render_to_response("accounts/login.html",template_var,)
+        username = request.POST.get("username")
+        template_var={"error":"must first register","username":username}                 
+    return render(request,"accounts/login.html",template_var,)
     
 	
 def logout(request):
-    return render_to_response('accounts/logout.html',)	
+    return render(request,'accounts/logout.html',)	
 	
